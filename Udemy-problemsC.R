5bca26cd2ed0ebb0882bf854874dd0777013773a
GaryFH <gary@hulbertbrothers.com>
2017-02-28 16:53:37 -0800

    more

--- Udemy-problemsC.R
+++ Udemy-problemsC.R
@@ -11,3 +11,12 @@ dm[c(1:(nrow(dm)*0.25)),]
 dm2<-which(USArrests[,2]>200|USArrests[,4]>20)
 USArrests[dm2,]
 
+        ##Challenge 3 - Murder>10 and Rape>15
+
+dm3<-which(USArrests[,1]>10,USArrests[,4]>15)
+USArrests[dm3,]
+
+        ##Challenge 4 - order descending murder
+
+dm4<-rev(order(USArrests[,1]))
+USArrests[dm4,]
